c6570ac10c3596ca1f2ee71f12a60a0fd7f1ec70
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-03-17 12:45:47 +0000

    Add code from lectures.

--- solution.py
+++ solution.py
@@ -61,7 +61,16 @@ def grid_values(grid):
         - keys: Box labels, e.g. 'A1'
         - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
     """
-    pass
+
+    values = []
+    all_digits = '123456789'
+    for c in grid:
+        if c == '.':
+            values.append(all_digits)
+        elif c in all_digits:
+            values.append(c)
+    assert len(values) == 81
+    return dict(zip(boxes, values))
 
 def display(values):
     """
@@ -71,19 +80,121 @@ def display(values):
     ----------
     values  : The sudoku in dictionary form.
     """
-    pass
+
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    """
+    Eliminates values from peers of each box with a single value.
+
+    Goes through all the boxes, and whenever there is a box with a single value,
+    eliminates this value from the set of values of all its peers.
+
+    Parameters
+    ----------
+    values  : The sudoku in dictionary form.
+
+    Returns
+    -------
+    Resulting Sudoku in dictionary form after eliminating values.
+    """
+
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 def only_choice(values):
-    pass
+    """
+    Finalizes all values that are the only choice for a unit.
+
+    Goes through all the units, and whenever there is a unit with a value that only fits in one box,
+    assigns the value to this box.
+
+    Parameters
+    ----------
+    values  : The sudoku in dictionary form.
+
+    Returns
+    -------
+    Resulting Sudoku in dictionary form after filling in only choices.
+    """
+
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    Iterates `eliminate()` and `only_choice()`. If at some point, there is a box with no available values,
+    returns `False`. If the sudoku is solved, returns the sudoku. If after an iteration of both functions, the sudoku
+    remains the same, function returns the sudoku.
+
+    Parameters
+    ----------
+    values  : The sudoku in dictionary form.
+
+    Returns
+    -------
+    The resulting sudoku in dictionary form.
+    """
+
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    """
+    Using depth-first search and propagation, tries all possible values.
+
+    Parameters
+    ----------
+    values  : The sudoku in dictionary form.
+
+    Returns
+    -------
+    The dictionary representation of the final sudoku grid. `False` if no solution exists.
+    """
+
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
